c65963f3d5dff3dc7023b45ad25e49a64f9347ea
João Pedro <joao99866@gmail.com>
2023-10-29 23:46:32 -0300

    Iniciando a Calculadora de Juros

---
+++ script/taxCalculator.js
@@ -0,0 +1,24 @@
+document.getElementById("calculateTax").addEventListener("click", function(){
+    const capitalValue = document.getElementById("capitalValue").value;
+    const feeValue = (document.getElementById("feeValue").value) /100 ;
+    const periodValue = document.getElementById("periodValue").value;
+
+    const typeTax = document.querySelector('input[name="tipoJuros"]:checked').value;
+
+    let output = "";
+
+    if(typeTax === 1){
+        const simpleTotal = capitalValue * (1 * feeValue * periodValue);
+        document.getElementById("montanteValue").value = ("R$ " + simpleTotal.toFixed(2)).toString();
+    }else if(typeTax === 2){
+        const compostTotal = capitalValue * (1 * feeValue) ** periodValue;
+        document.getElementById("montanteValue").value = ("R$ " + compostTotal.toFixed(2)).toString();
+    }
+
+    console.log(capitalValue);
+    console.log(feeValue);
+    console.log(periodValue);
+    console.log(typeTax);
+    console.log(output);
+});
+
